a25f8edc58062e8022948de82bf521ed1059cb3b
Madison Brown <59658823+mbcodes29@users.noreply.github.com>
2020-10-02 13:37:47 -0400

    edits

--- modules/app/app.js
+++ modules/app/app.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const cors = require('cors')
+const helmet = require('helmet')
+const morgan = require('morgan')
+const { NODE_ENV } = require('./config')
+const peopleRouter = require('../people/people.router')
+const petseRouter = require('../pets/pets.router')
 
 const app = express()
 
-const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
 app.use(morgan(morganSetting))
 
+app.use(helmet())
 app.use(cors())
 
 app.use('/people', require('../people/people.router'))
--- modules/queue/Queue.js
+++ modules/queue/Queue.js
@@ -1,16 +1,45 @@
+class _Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
 class Queue {
   constructor() {
-    // Set initial data.
+    this.first = null;
+    this.last = null;
+    this.length = 0;
   }
 
   enqueue(data) {
-    // Add some data to the queue.
+    const node = new _Node(data);
+    
+    if (this.first === null) {
+      this.first = node;
+    }
+
+    if (this.last) {
+      this.last.next = node;
+    }
+
+    this.last = node;  
+    this.length += 1;
   }
 
   dequeue() {
-    // Remove some data from the queue.
-  }
+    if (this.first === null) {
+      return;
+    }
+    const node = this.first;
+    this.first = this.first.next;
 
+    if (node === this.last) {
+      this.last = null;
+    }
+    this.length -= 1;
+    return node.value;
+  }
   show() {
     // Return the next item in the queue.
   }
