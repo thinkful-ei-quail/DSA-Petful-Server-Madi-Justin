0f526082018ed1d6ffcee331c0fdf6c0029c178d
Madison Brown <59658823+mbcodes29@users.noreply.github.com>
2020-10-02 12:28:02 -0400

    Deployment prep

--- modules/app/app.js
+++ modules/app/app.js
@@ -3,9 +3,22 @@ const cors = require('cors')
 
 const app = express()
 
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
+
 app.use(cors())
 
 app.use('/people', require('../people/people.router'))
 app.use('/pets', require('../pets/pets.router'))
+
+app.use(function errorHandler(error, req, res, next) {
+  let response;
+  if (NODE_ENV === 'production') {
+    response = { error: { message: "server error" } };
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
+});
 
 module.exports = app
